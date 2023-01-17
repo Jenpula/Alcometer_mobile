2d67b7a8439ecc7bce28613f6f4aae9805a32d6b
Annala <n1anje00@students.oamk.fi>
2023-01-17 14:44:19 +0200

    Styling

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput, ScrollView, StyleSheet, Text, View, Switch, ImageBackground} from 'react-native';
+import { useState } from 'react';
+import Styles from './Styles/Styles.js';
+import NumericInput from 'react-native-numeric-input';
+import { RadioButton } from 'react-native-paper';
+
 
 export default function App() {
+
+  const [hours,setHours] = useState(0);
+  const [weight, setWeight] = useState(90);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [result, setResult] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const [radioval, setRadioval] = useState('female')
+
+
+  function calculate() {
+
+    const litres = bottles * 0.33;
+    const burning = weight / 10;
+    const grams = litres * 8 * 4.5
+    const left = grams - (burning * time)
+    let alcohol = 0;
+
+    if(gender === 'male') {
+      alcohol = left / (weight * 0.7);
+    } else {
+      alcohol = left / (weight * 0.6);
+    }
+    if(alcohol < 0 ) {
+      setResult(0);
+    } else {
+      setResult(alcohol);
+    }
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView>
+    <View style={Styles.onoff}>
+      <Text>On/Off</Text>
+      <Switch
+        value={isOn}
+        onValueChange= {newValue => setIsOn(newValue)}
+        thumbColor='white'
+        trackColor={{false: 'white', true: 'red'}} 
+        />
+      </View>
+    <View style={Styles.container}>
+      <Text style={Styles.header}>Alcometer</Text>
+      <Text style={Styles.label}>Weight:</Text>
+      <TextInput 
+        keyboardType='number-pad' 
+        //onChangeText={ t => setNum1(t) }
+        style={Styles.textInput}
+      />
+      <Text style={Styles.label}>Bottles</Text>
+        <NumericInput onChange={v => setBottles(v)}/>
+
+      <Text style={Styles.label}>Hours</Text>
+        <NumericInput onChange={v => setHours(v)}/>
+
+      <RadioButton.Group onValueChange={newValue => setRadioval(newValue)} value={radioval}>
+      <View style={Styles.radiostyle}>
+        <RadioButton value='Female'/>
+        <Text>Female</Text>
+      </View>
+      <View style={Styles.radiostyle}>
+        <RadioButton value='Male'/>
+        <Text>Male</Text>
+      </View>
+      </RadioButton.Group>
+
+      <Button style={Styles.button} title='Calculate'></Button>
+      <Text></Text>
     </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ Styles/Styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'space-evenly',
+        margin: 10,
+        padding: 10,
+        backgroundColor: ''
+      
+    },
+    textInput:{
+        padding: 5,
+        borderWidth:1,
+        marginBottom: 10
+    },
+    label:{
+        fontSize: 18,
+        fontWeight: "bold",
+        padding: 10
+    },
+    result:{
+        color: '#b00000',
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    header: {
+        fontSize: 35,
+       alignContent: "center",
+       padding: 40,
+       margin: 20
+    },
+    onoff: {
+        alignItems: 'center',
+        margin: 20,
+        padding: 15
+    },
+    radiostyle: {
+        padding: 10,
+        margin: 2
+    },
+    button: {
+        bcolor: 'white'
+    }
+});
+  
