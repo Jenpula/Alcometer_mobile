facf23ec62ca7382df18517b4e68c5ff86a9075a
Annala <n1anje00@students.oamk.fi>
2023-02-06 10:28:12 +0200

    Style changes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, TextInput, ScrollView, StyleSheet, Text, View, Switch, ImageBackground} from 'react-native';
+import { Button, TextInput, ScrollView, StyleSheet, Text, View, Switch, TouchableOpacity} from 'react-native';
 import { useState } from 'react';
-import Styles from './Styles/Styles.js';
+import {lightStyles, darkStyles, Styles} from './Styles/Styles.js';
 import NumericInput from 'react-native-numeric-input';
 import { RadioButton } from 'react-native-paper';
 
@@ -14,9 +14,9 @@ export default function App() {
   const [gender, setGender] = useState('male');
   const [result, setResult] = useState(0);
   const [isOn, setIsOn] = useState(false);
-  
 
-  const color = isOn ? Styles.darkColor : Styles.lightColor;
+
+  const color = isOn ? darkStyles : lightStyles;
 
   function calculate() {
 
@@ -44,50 +44,49 @@ export default function App() {
   }
  
   return (
-    <ScrollView style={color}>
-    <View style={Styles.container}>
+    <View style={color.container}>
       <Switch
         value={isOn}
         onValueChange= {newValue => setIsOn(newValue)}
         />
-     
-      <Text style={Styles.header}>Alcometer</Text>
-      <Text style={Styles.label}>Weight:</Text>
+     <ScrollView>
+      <Text style={color.header}>Alcometer</Text>
+      <Text style={color.label}>Weight:</Text>
       <TextInput 
         keyboardType='number-pad' 
         value={weight}
         onChangeText={v => setWeight(parseInt(v))}
-        style={Styles.textInput}
+        style={color.textInput}
       />
-      <Text style={Styles.label}>Bottles</Text>
+      <Text style={color.label}>Bottles</Text>
         <NumericInput
         value={bottles} 
         minValue={0} 
         onChange={e => setBottles(e)}/>
 
-      <Text style={Styles.label}>Hours</Text>
-        <NumericInput
-        style={Styles.nInput}
+      <Text style={color.label}>Hours</Text>
+        <NumericInput style={color.NumericInput}
         value={hours}
         minValue={0} 
         onChange={(e) => {setHours(e)}}/>
 
       <RadioButton.Group value={gender} onValueChange={newValue => setGender(newValue)}>
-      <View style={Styles.radiostyle}>
+      <View style={color.radiostyle}>
         <RadioButton value='female'/>
         <Text>Female</Text>
       </View>
-      <View style={Styles.radiostyle}>
+      <View style={color.radiostyle}>
         <RadioButton value='male'/>
         <Text>Male</Text>
       </View>
       </RadioButton.Group>
-
-      <Button style={Styles.button} title='Calculate' onPress={calculate}></Button>
-      <Text style={Styles.result}>{result.toFixed(2)}</Text>
+      <TouchableOpacity>
+        <Text style={color.button} title='Calculate' onPress={calculate}>Calculate</Text>
+        <Text style={color.result}>{result.toFixed(2)}</Text>
+      </TouchableOpacity>
+      </ScrollView>
       </View>
-     
-    </ScrollView>
+   
   );
  
 }
--- Styles/Styles.js
+++ Styles/Styles.js
@@ -1,13 +1,13 @@
 import { StyleSheet } from "react-native";
 
 
-export default StyleSheet.create({
+export const lightStyles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'stretch',
         justifyContent: 'space-evenly',
-        margin: 10,
-        padding: 10,
+        padding: 20,
+        backgroundColor: 'white'
           
     },
     
@@ -15,7 +15,8 @@ export default StyleSheet.create({
         padding: 5,
         borderWidth:1,
         marginBottom: 10,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        textAlign: 'center'
        
     },
 
@@ -26,9 +27,12 @@ export default StyleSheet.create({
         alignSelf: 'center',
        
     },
-    nInput: {
+    NumericInput: {
         textAlign: 'center',
-        alignSelf: 'center'
+        alignSelf: 'center',
+        textColor: 'white',
+        borderRadius: 10
+      
     },
    
     result:{
@@ -42,29 +46,106 @@ export default StyleSheet.create({
         fontSize: 35,
        padding: 40,
        margin: 20,
-       alignSelf: 'center'
+       alignSelf: 'center',
+       color: '#4aaac8'
+
     },
   
     radiostyle: {
         padding: 5,
         alignSelf: 'center',
+        color: 'black'
         
     },
     button: {
         color: 'white',
+        backgroundColor: '#4aaac8',
+        padding: 10,
+        margin: 10,
+        alignSelf: 'center',
+        width: 200,
+        textAlign: 'center',
+        fontSize: 18,
+        borderRadius: 10,
+        fontWeight: 'bold'
+    },
+
+});
+
+export const darkStyles = StyleSheet.create({
+
+ container: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'space-evenly',
+        padding: 20,
+        backgroundColor: '#2099b4',
+          
+    },
+    
+    textInput: {
+        padding: 5,
+        borderWidth:1,
+        marginBottom: 10,
         backgroundColor: 'white',
+        textAlign: 'center',
+        color: 'white'
+       
+    },
+
+    label: {
+        fontSize: 18,
+        fontWeight: "bold",
+        padding: 10,
+        alignSelf: 'center',
+        color: 'white'
+       
+    },
+    NumericInput: {
+        textAlign: 'center',
+        alignSelf: 'center',
+        textColor: 'white',
+        borderRadius: 10, 
+        itemSelf: 'center',
+        color: 'white'
+      
+    },
+   
+    result:{
+        color: '#ffffff',
+        fontSize: 18,
+        fontWeight: "bold",
+        alignSelf: 'center',
         padding: 10
     },
+    header: {
+        fontSize: 35,
+       padding: 40,
+       margin: 20,
+       alignSelf: 'center',
+       color: 'white'
 
-    darkColor: {
-        backgroundColor: '#afb4b4',
-    
+    },
+  
+    radiostyle: {
+        padding: 5,
+        alignSelf: 'center',
+        color: 'white'
+        
+    },
+    button: {
+        color: 'black',
+        backgroundColor: 'white',
+        padding: 10,
+        margin: 10,
+        alignSelf: 'center',
+        width: 200,
+        textAlign: 'center',
+        fontSize: 18,
+        borderRadius: 10,
+        fontWeight: 'bold'
     },
 
-    lightColor: {
-        backgroundColor: 'white'
-    }
- 
 });
 
 
